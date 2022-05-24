f7b18e4981fdf068a37ff5eba9cf57ddee1a5559
胡恒东 <1620175472@qq.com>
2022-05-24 10:52:23 +0800

    add pinia

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 import routes from './routes'
+import { createPinia } from 'pinia'
 import 'uno.css'
 
 const router = createRouter({
   routes,
   history: createWebHashHistory()
 });
-const app = createApp(App)
+const app = createApp(App);
 app.use(router);
-
-app.mount('#app')
+app.use(createPinia());
+app.mount('#app');
---
+++ src/stores/MainStore.ts
@@ -0,0 +1,10 @@
+import { defineStore } from "pinia";
+
+export const useStore = defineStore("mainStore", {
+  // state
+  state: () => {
+    return {}
+  }
+  // actions
+  // getters
+})
